c4592de0bbba7bf6d92294ddfce682151386295d
Anand Balakrishnan <anandbala1597@gmail.com>
2024-12-21 16:33:25 -0800

    fix: fzf-lua keymaps to resemble telescope

--- after/plugin/fzf.lua
+++ after/plugin/fzf.lua
@@ -1,4 +1,5 @@
 local command = vim.api.nvim_create_user_command
+local actions = require "fzf-lua.actions"
 
 vim.schedule(function()
   vim.cmd [[:packadd fzf-lua]]
@@ -15,6 +16,33 @@ vim.schedule(function()
       col = 0.5,
       preview = {
         scrollchars = { "┃", "" },
+      },
+    },
+    actions = {
+      files = {
+        -- instead of the default action 'actions.file_edit_or_qf'
+        -- it's important to define all other actions here as this
+        -- table does not get merged with the global defaults
+        ["default"] = actions.file_edit,
+        ["enter"] = actions.file_edit_or_qf,
+        ["ctrl-x"] = actions.file_split,
+        ["ctrl-v"] = actions.file_vsplit,
+        ["ctrl-t"] = actions.file_tabedit,
+        ["alt-q"] = actions.file_sel_to_qf,
+      },
+    },
+    buffers = {
+      keymap = { builtin = { ["<C-d>"] = false } },
+      actions = { ["ctrl-x"] = false, ["ctrl-d"] = { actions.buf_del, actions.resume } },
+    },
+    files = {
+      fzf_opts = {
+        ["--history"] = vim.fn.stdpath "data" .. "/fzf-lua-files-history",
+      },
+    },
+    grep = {
+      fzf_opts = {
+        ["--history"] = vim.fn.stdpath "data" .. "/fzf-lua-grep-history",
       },
     },
   }
