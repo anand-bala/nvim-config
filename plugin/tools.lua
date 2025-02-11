fed5bec119d3f64c8cc9500e6926960f551db76b
Anand Balakrishnan <anandbala1597@gmail.com>
2025-02-11 14:56:19 -0800

    clean up blink.cmp

--- plugin/tools.lua
+++ plugin/tools.lua
@@ -132,16 +132,7 @@ require("blink.cmp").setup {
         },
       },
     },
-    menu = {
-      auto_show = function(ctx) return ctx.mode ~= "cmdline" end,
-      -- draw = {
-      --   columns = {
-      --     -- { "label", "label_description", gap = 1 },
-      --     -- { "kind_icon", "kind", gap = 1 },
-      --   },
-      -- },
-    },
-    -- documentation = { auto_show = true },
+    menu = { auto_show = function(ctx) return ctx.mode ~= "cmdline" end },
   },
 }
 
