ddc459ea3209122ed6577cb2e6259c7351e063fd
Anand Balakrishnan <anandbala1597@gmail.com>
2025-05-20 15:15:33 -0700

    feat: add option for mason autoinstalling

--- plugin/tools.lua
+++ plugin/tools.lua
@@ -140,18 +140,22 @@ end
 require("mason").setup()
 require("mason-lspconfig").setup()
 
--- autocmd({ "FileType" }, {
---   group = augroup("Auto-install tools", { clear = true }),
---   pattern = "*",
---   callback = function(ctx)
---     local tools = require("_utils").get_configured_tools(ctx.buf)
---     local to_install = {}
---     for name, info in pairs(tools) do
---       if not info.available then table.insert(to_install, name) end
---     end
---     require("_utils").mason_install(to_install)
---   end,
--- })
+vim.g.mason_autoinstall = vim.g.mason_autoinstall or false
+autocmd({ "FileType" }, {
+  group = augroup("Auto-install tools", { clear = true }),
+  pattern = "*",
+  callback = function(ctx)
+    if not vim.g.mason_autoinstall then
+      return
+    end
+    local tools = require("_utils").get_configured_tools(ctx.buf)
+    local to_install = {}
+    for name, info in pairs(tools) do
+      if not info.available then table.insert(to_install, name) end
+    end
+    require("_utils").mason_install(to_install)
+  end,
+})
 
 -- Formatting
 vim.g.formatting_opts = vim.g.formatting_opts or {}
