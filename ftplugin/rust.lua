b425de6213b2602c90401aad8dd1ce3eba2c56cf
Anand Balakrishnan <anandbala1597@gmail.com>
2025-05-28 09:19:17 -0700

    fix: just use rustaceanvim default config

--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -1,33 +1,8 @@
-vim.opt.textwidth = 88
+---@module "rustaceanvim"
 
-vim.g.rustaceanvim = {
-  server = {
-    settings = {
-      ["rust-analyzer"] = {
-        cargo = {
-          features = "all",
-        },
-        -- Add clippy lints for Rust.
-        checkOnSave = true,
-        -- Use nightly for rustfmt
-        rustfmt = {
-          extraArgs = "+nightly",
-        },
-        -- Use clippy for checking
-        check = {
-          command = "clippy",
-          features = "all",
-        },
-        -- Enable procedural macro support
-        procMacro = {
-          enable = true,
-        },
-      },
-    },
-  },
-}
+vim.opt.textwidth = 88
 
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(event) vim.lsp.inlay_hint.enable(false, { bufnr = event.buf }) end,
-  desc = "disable LSP inlay hints for Rust. Use rust-tools.",
+  desc = "disable LSP inlay hints for Rust. Use rustaceanvim.",
 })
