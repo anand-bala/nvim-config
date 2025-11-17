5450abbc7e4708c5299ca19447ed12d6bab58d83
Anand Balakrishnan <anandbala1597@gmail.com>
2025-11-17 13:54:05 -0600

    move lsp configurations to after

--- after/lsp/ty.lua
+++ after/lsp/ty.lua
@@ -1,5 +1,7 @@
 ---@type vim.lsp.Config
 return {
+  cmd = { "ty", "server" },
+  filetypes = { "python" },
   settings = {
     -- ty = { disableLanguageServices = false },
     ty = {
@@ -11,5 +13,16 @@ return {
   },
   init_options = {
     logFile = vim.fs.joinpath(vim.fn.stdpath "log", "lsp", "ty.log"),
+  },
+  root_markers = {
+    "ty.toml",
+    "pyproject.toml",
+    "setup.py",
+    "setup.cfg",
+    "requirements.txt",
+    "Pipfile",
+    "pyrightconfig.json",
+    ".jj",
+    ".git",
   },
 }
